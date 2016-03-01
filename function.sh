a64cf26b8265b2b18cab3f89cfd70193f8df697d
photous <raryat@googlemail.com>
2016-03-01 17:06:53 +0000

    Nest everything and make it shorter

--- function.sh
+++ function.sh
@@ -1,5 +1,4 @@
-
-function mkapp {  mkdir -p $1/{'static','static/css','static/js','static/img','templates'}; }
-
 # The following also works, but the above is shorter
 #function mkapp { mkdir "$1" "$1/static" "$1/templates" "$1/static/css" "$1/static/js" "$1/static/img"; }
+#function mkapp {  mkdir -p $1/{'static','static/css','static/js','static/img','templates'}; }
+function mkapp { mkdir -p "$1"/{static/{js,img,css},templates}; }
