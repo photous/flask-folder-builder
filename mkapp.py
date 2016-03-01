6572b5a4a8f1f45468b2858d85be7c96ed6bff0e
Tim Shaffer <timshaffer@me.com>
2016-03-01 08:21:53 -0500

    Removed repetitive .format() calls

--- mkapp.py
+++ mkapp.py
@@ -4,13 +4,11 @@ import sys
 
 
 def mkapp(app_name):
-    dirs = [app_name,
-            '{}/static'.format(app_name),
-            '{}/static/css'.format(app_name),
-            '{}/static/js'.format(app_name) ,
-            '{}/static/img'.format(app_name),
-            '{}/templates'.format(app_name)]
+    dirs = ['{}', '{}/static', '{}/static/css', '{}/static/js', '{}/static/img', '{}/templates']
     for d in dirs:
-        os.mkdir(d)
-app = sys.argv[1]
-mkapp(app)
+        os.mkdir(d.format(app_name))
+
+
+if __name__ == '__main__':
+    app = sys.argv[1]
+    mkapp(app)
